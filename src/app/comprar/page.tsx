7cd58d593d75fd02b6b1f893ce6c7f0f5d3d2687
Leonardo <leonardonaime2@gmail.com>
2024-08-02 16:55:15 -0300

    criação de páginas na aba comprar

--- src/app/comprar/page.tsx
+++ src/app/comprar/page.tsx
@@ -22,18 +22,28 @@ type Car = {
 
 const Comprar = () => {
     const [cars, setCars] = useState<Car[]>([])
+    const [pageNumber, setPageNumber] = useState(0);
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPageNumber, setCurrentPageNumber] = useState(0);
     const router = useRouter();
 
     useEffect(() => {
         const fetchCars = async () => {
-            const useCars = await allCars()
+            const useCars = await allCars(pageNumber)
             if (useCars) {
                 // @ts-ignore
                 setCars(useCars.data.content)
+                // @ts-ignore
+                setTotalPages(useCars.data.totalPages)
             }
         }
         fetchCars()
-    }, []);
+    }, [pageNumber]);
+
+    const handlePageChange = (pageNumber: number) => {
+      setPageNumber(pageNumber)
+      setCurrentPageNumber(pageNumber)
+    }
 
     return (
       <div className="bg-[#EEEEEE] flex min-h-screen flex-col">
@@ -43,7 +53,7 @@ const Comprar = () => {
         <div className="flex">
           <div className="flex justify-center flex-grow mt-10 mb-10">
             {cars.length > 0 ? (
-              <div className="grid grid-cols-3 gap-10">
+              <div className="grid grid-cols-4 gap-10">
                 {cars.map((car, index) => (
                   <div
                     key={index}
@@ -58,6 +68,17 @@ const Comprar = () => {
               </div>
             )}
           </div>
+        </div>
+        <div className="flex justify-center mb-10"> 
+            {Array.from({ length: totalPages }, (_, i) => i).map((pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              className={`mx-2 ${pageNumber === currentPageNumber ? ' text-black' : ' text-gray-500'}`}
+            >
+            {pageNumber + 1}
+            </button>
+            ))}
         </div>
         <div>
           <DefaultFooter></DefaultFooter>
