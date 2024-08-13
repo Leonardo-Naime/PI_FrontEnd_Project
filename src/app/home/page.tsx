61b463e2b19a1403c7fab7ba116230071004f649
Leonardo Naime <leonardonaime2@gmail.com>
2024-08-13 14:54:14 -0300

    adição destaques

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,9 +14,14 @@ const Home = () => {
         <div>
           <Header></Header>
         </div>
-        <div className="bg-[#EEEEEE] flex w-full justify-center h-full pt-4 pb-4">
-           <CarouselSize>
+        <div className="bg-[#EEEEEE] pt-20 pb-20">
+          <div className="mb-5">
+            <h2 className="text-center text-3xl text-zinc-500 font-sans font-light">Destaques</h2>
+          </div>
+          <div className="flex w-full justify-center h-full">
+            <CarouselSize>
             </CarouselSize>
+          </div>
         </div>
         <div>
           <DefaultFooter></DefaultFooter>
