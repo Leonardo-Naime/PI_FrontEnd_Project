fbb146068c1cada39b75b861e6e0116af8042799
Leonardo Naime <leonardonaime2@gmail.com>
2024-08-14 19:52:22 -0300

    inicialização personalização footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,16 @@
 const DefaultFooter = () => {
     return(
     <main>
-      <div className="bg-[#64BCED] h-10"></div>
-      <div className="bg-[#254354] h-72"></div>
-      <div className="bg-[#64BCED] h-10"></div>
-    </main>
+      <div className="bg-[#64BCED] h-10">
 
+      </div>
+      <div className="bg-[#254354] h-72">
+
+      </div>
+      <div className="bg-[#64BCED] h-10">
+        <p className=" text-white text-center pt-2">Rodapé - © 2024 Meu Site</p>
+      </div>
+    </main>
     );
 }
 
