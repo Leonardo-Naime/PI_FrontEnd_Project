157b4dd02ed4b09024ca879ace346c5fefd71f48
Leonardo <leonardonaime2@gmail.com>
2024-08-14 17:20:43 -0300

    criação aba sobre

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Header from "@/components/header/header"
+import DefaultFooter from "@/components/footer/footer"
+
+
+const Sobre = () => {
+
+  return (
+    <main className="">
+      <div className="bg-[#EEEEEE]">
+        <div>
+          <Header></Header>
+        </div>
+        <div>
+            <h2>teste</h2>
+        </div>
+        <div>
+          <DefaultFooter></DefaultFooter>
+        </div>
+      </div>
+    </main>
+  );
+}
+  
+  export default Sobre
